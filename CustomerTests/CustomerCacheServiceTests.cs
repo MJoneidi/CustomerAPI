3dbc355603039041702fe8d0e1c02cb74db05567
Mohammad Joneidi <mjoneidi@accessholding.com>
2021-07-03 08:58:48 +0200

    added base test project

---
+++ CustomerTests/CustomerCacheServiceTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomerTests
+{
+    [TestClass]
+    public class CustomerCacheServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
